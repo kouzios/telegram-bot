f5411e1b51ec214f567e91da81e392a23c57422c
mkouzios <mkouzios@hotmail.com>
2020-01-25 20:17:58 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const TelegramBot = require('node-telegram-bot-api');
+var logger = require('winston');
+var auth = require('./auth.json');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+const bot = new TelegramBot(auth.token, {polling: true});
+
+// Matches "/meme [whatever]"
+bot.onText(/\/meme (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp + " is a big ol' meme");
+});
+
+// // Listen for any kind of message. There are different kinds of
+// // messages.
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+
+//   // send a message to the chat acknowledging receipt of their message
+//   bot.sendMessage(chatId, 'Received your message');
+// });
